e1bc21edb2a1a5b3fac2d37789d68a681fe73742
adityaparab04 <adityaparab04@gmail.com>
2022-03-06 18:55:34 -0500

    Halfway through

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from 'react-router-dom'
+import AllMeetupsPage from './pages/AllMeetUps';
+import NewMeetupPage from './pages/NewMeetUp';
+import Favorites from './pages/Favorites';
+import Layout from './components/layout/Layout'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  return ( 
+    <Layout>
+      <Routes>
+        <Route path='/' element={<AllMeetupsPage/>}/>
+        <Route path='/new-meetup' element={<NewMeetupPage/>}/>
+        <Route path='favorites' element={<Favorites/>}/>
+      </Routes>
+    </Layout> 
   );
 }
 
